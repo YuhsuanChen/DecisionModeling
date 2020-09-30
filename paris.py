4ea82c0c6ee9151b0346f1b9de8b476f9caf31bf
Alice <61308780+YuhsuanChen@users.noreply.github.com>
2020-09-30 15:39:33 +0200

    Organize

--- paris.py
+++ paris.py
@@ -46,7 +46,7 @@ def VisitList(p1, p2, p3, p4, p5):
     """Preference constraint"""
     if p1 == 1:
         prob += X[3] == X[4]  # Preference 1 (MO,JT)
-        prob += X[6] == X[7]   # Preference 1 (CN,CP)
+        prob += X[6] == X[7]  # Preference 1 (CN,CP)
         prob += X[1] == X[6] == X[7] == X[9]  # Preference 1 (SC,ML,CP,CN)
     if p2 == 1:
         prob += X[0] + X[5] == 2  # C-Preference2
@@ -100,3 +100,35 @@ def Comparison():
             compare.append("True")
     df2 = df.assign(Same=compare)
     df2.to_csv("answer.csv", index=False)
+
+
+VisitList(0, 0, 0, 0, 0)  # VisitList-1
+"""
+The 5 parameter represents preference Mr.Doe considered.
+For example: VisitList(1, 0, 0, 0, 0)  # Preference 1: Near Attraction
+             VisitList(0, 1, 0, 0, 0)  # Preference 2: Absolutely visit (TE) and (CA)
+             VisitList(0, 0, 1, 0, 0)  # Preference 3 : If visits CN then he will not visit SC
+             VisitList(0, 0, 0, 1, 0)  # Preference 4: Absolutely visit (TM)
+             VisitList(0, 0, 0, 0, 1)  # Preference 5: If visits ML then must visit the CP
+For question (b) to (l), the answer will be in the csv file that generate after running VisitList
+"""
+#
+VisitList(1, 0, 0, 0, 0)  # Preference 1: Near Attraction
+VisitList(0, 1, 0, 0, 0)  # Preference 2: Absolutely visit (TE) and (CA)
+VisitList(0, 0, 1, 0, 0)  # Preference 3 : If visits CN then he will not visit SC
+VisitList(0, 0, 0, 1, 0)  # Preference 4: Absolutely visit (TM)
+VisitList(0, 0, 0, 0, 1)  # Preference 5: If visits ML then must visit the CP
+VisitList(1, 1, 0, 0, 0)  # (b) Preference1 & 2
+VisitList(1, 0, 1, 0, 0)  # (c) Preference1 & 3
+VisitList(1, 0, 0, 1, 0)  # (d) Preference1 & 4
+VisitList(0, 1, 0, 0, 1)  # (e) Preference 2 & 5
+VisitList(0, 0, 1, 1, 0)  # (f) Preference 3 & 4
+VisitList(0, 0, 0, 1, 1)  # (g) Preference 4 & 5
+VisitList(1, 1, 0, 1, 0)  # (h) Preference 1 & 2 & 4
+VisitList(0, 1, 1, 0, 1)  # (i) Preference 2 & 3 & 5
+VisitList(0, 1, 1, 1, 1)  # (j) Preference 2 & 3 & 4 & 5
+VisitList(1, 1, 0, 1, 1)  # (k) Preference 1 & 2 & 4 & 5,
+VisitList(1, 1, 1, 1, 1)  # (l) Preference 1 & 2 & 3 & 4 & 5
+
+# (m) Is the solution ListVisit1 different to these solutions founded above (with the combination of preferences) ?
+Comparison()
